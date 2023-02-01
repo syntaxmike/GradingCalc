066a35c46722095a73e5d81521952c137e457ede
Michael A. Oceguera <MOceguera@collectors.com>
2023-01-31 21:47:16 -0800

    github pages edit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/GradingCalc/' : '',
+  images: {
+    unoptimized: true,
+  },
 }
 
+
 module.exports = nextConfig
+
