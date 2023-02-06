83ecf00beb399fe3ca818136290222d461c30897
Michael A. Oceguera <MOceguera@collectors.com>
2023-02-05 18:17:39 -0800

    Update

---
+++ pages/components/Form.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import { Inter } from '@next/font/google';
+import Image from 'next/image'
+
+const inter = Inter({ subsets: ['latin'] })
+
+
+const CourseInput = () => {
+
+    const [fValue, setFValue] = useState(0);
+    const [sValue, setSValue] = useState(0);
+
+    let percent = Math.ceil(((Math.max(fValue, sValue)/(fValue + sValue)) * 100));
+    
+    function handleEnter(e: any) {
+      if (e.keyCode === 13) {
+        const form = e.target.form;
+        const index = Array.prototype.indexOf.call(form, e.target);
+        form.elements[index + 1].focus();
+        e.preventDefault();
+      }
+    }
+
+    function handleReset() {
+      setFValue(0);
+      setSValue(0);
+    }
+
+  return (
+    <form>
+    <Image className="object-center" src="/psa.png" alt="psa" width="200" height="200" />
+    <h1 className="py-4 underline underline-offset-1 text-lg">Centering Calculator</h1>
+      <label>
+        Edge A {isNaN(fValue/(fValue + sValue)) ? '' : (fValue/(fValue + sValue) * 100).toFixed(1) + '%'}
+      </label>
+      <div className="py-2">
+        <input 
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="number" 
+            min="0" 
+            pattern="[0-9]*" 
+            onKeyDown={handleEnter} 
+            onFocus={(e) => e.target.select()} 
+            onChange={ e => 
+                e.target.value ? setFValue(parseInt(e.target.value)) : null
+            } 
+            required
+            value={'' || Math.max(0, fValue)}
+        />
+      </div>
+      <label>
+        Edge B {isNaN(sValue/(fValue + sValue)) ? '' : (sValue/(fValue + sValue) * 100).toFixed(1) + '%'}
+      </label>
+      <div className="py-2">
+        <input 
+            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="number" 
+            min="0" 
+            pattern="[0-9]*" 
+            onKeyDown={handleEnter} 
+            onFocus={(e) => e.target.select()} 
+            onChange={ e => 
+                e.target.value ? setSValue(parseInt(e.target.value)): null
+            } 
+            required
+            value={'' || Math.max(0, sValue)}
+        />
+      </div>
+      <div className="px-16 py-2">
+        <input
+            className='bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded'
+            type="reset" 
+            onClick={handleReset} 
+            value="Reset" />
+      </div>
+      <p className="px-2 py-2 text-2xl text-center">Centering Grade: 
+            {percent >= 50 && percent <= 55 && 
+                <span className="text-green-500 text-5xl px-2">10</span>
+            }
+            {percent >= 56 && percent <= 60 && 
+                <span className="text-red-400 text-5xl px-2">9</span>
+            }
+            {percent >= 61 && percent <= 65 && 
+                <span className="text-yellow-400 text-5xl px-2">8</span>
+            }
+            {percent >= 66 && percent <= 70 && 
+                <span className="text-blue-400 text-5xl px-2">7</span>
+            }
+            {percent >= 71 && percent <= 80 && 
+                <span className="text-neutral-400 text-5xl px-2">6</span>
+            }
+            {percent >= 81 && percent <= 85 && 
+                <span className="text-gray-200 text-5xl px-2">5</span>
+            }
+            {percent > 85 && 
+                <span className="text-gray-300 text-5xl px-2">4</span>
+            }
+      </p>
+    </form>
+  );
+};
+
+export default CourseInput;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,9 @@
 import Head from 'next/head'
 import React, {useEffect, useRef, useState } from 'react';
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-const initalFValue = 0;
-const initalSValue = 0;
+import Form from './components/Form';
 
 const Home = () => {
-
-  const [fValue, setFValue] = useState(initalFValue);
-  const [sValue, setSValue] = useState(initalSValue);
-
-  let percent = Math.ceil(((Math.max(fValue, sValue)/(fValue + sValue)) * 100));
-  
-  function handleEnter(event: any) {
-    if (event.keyCode === 13) {
-      const form = event.target.form;
-      const index = Array.prototype.indexOf.call(form, event.target);
-      form.elements[index + 1].focus();
-      event.preventDefault();
-    }
-  }
-
-  // const edgeAFocus = useRef(null);
-
-  // useEffect(() => {
-  //   if (edgeAFocus.current) {
-  //     edgeAFocus.current.focus();
-  //   }else {
-
-  //   }
-  // }, []);
-
-  function handleReset() {
-    setFValue(initalFValue);
-    setSValue(initalSValue);
-  }
 
   return (
     <>
@@ -46,35 +13,7 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={styles.main}>
-        <form>
-        <h1 className="p-8">Centering Calculator</h1>
-          <label>
-            Edge A 
-          </label>
-          <div className="py-2">
-            <input type="number" onKeyDown={handleEnter} value={fValue} onFocus={(e) => e.target.select()} onChange={e => e.target.value ? setFValue(parseInt(e.target.value)) : 0}/>
-            <span className="p-4">{(fValue/(fValue + sValue) * 100).toFixed(1)} %</span>
-          </div>
-          <label>
-            Edge B 
-          </label>
-          <div className="py-2">
-            <input type="number" onKeyDown={handleEnter} value={sValue} onFocus={(e) => e.target.select()} onChange={e => e.target.value ? setSValue(parseInt(e.target.value)): 0} />
-            <span className="p-4">{(sValue/(fValue + sValue) * 100).toFixed(1)} %</span>
-          </div>
-          <div className="px-16 py-2">
-            <input type="reset" onClick={handleReset} className='bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded' value="Reset" />
-          </div>
-          <p className="px-2 py-2 text-2xl">Centering Grade: 
-              {percent >= 50 && percent <= 55 && <span className="text-green-600 text-5xl px-2">10</span>}
-              {percent >= 56 && percent <= 60 && <span className="text-red-600 text-5xl px-2">9</span>}
-              {percent >= 61 && percent <= 65 && <span className="text-yellow-500 text-5xl px-2">8</span>}
-              {percent >= 66 && percent <= 70 && <span className="text-blue-600 text-5xl px-2">7</span>}
-              {percent >= 71 && percent <= 80 && <span className="text-neutral-600 text-5xl px-2">6</span>}
-              {percent >= 81 && percent <= 85 && <span className="text-gray-500 text-5xl px-2">5</span>}
-              {percent > 85 && <span className="text-gray-500 text-5xl px-2">4</span>}
-          </p>
-        </form>
+          <Form />
       </main>
     </>
   )
